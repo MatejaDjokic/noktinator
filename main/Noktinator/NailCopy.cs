f62811b6236a54290725cd3ec3bb69a3ce6d5383
P4lm4D3v <matejadjokic23@gmail.com>
2024-03-29 15:18:08 +0100

    v0.1.11

---
+++ main/Noktinator/NailCopy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+
+namespace Noktinator
+{
+    class NailCopy
+    {
+        public NailShapes nailShape = NailShapes.Almond;
+        public NailPatterns nailPattern = NailPatterns.Leaf;
+        public Color nailColor = Color.White;
+        public Color patternColor = Color.Black;
+
+        public NailCopy() { }
+
+        public void setShape(NailShapes newShape)
+        {
+            this.nailShape = newShape;
+        }
+
+        public void setPattern(NailPatterns newPattern)
+        {
+            this.nailPattern = newPattern;
+        }
+
+        public void setNailColor(Color newNailColor)
+        {
+            this.nailColor = newNailColor;
+        }
+
+        public void setPatternColor(Color newPatternColor)
+        {
+            this.patternColor = newPatternColor;
+        }
+
+        public Bitmap getImage()
+        {
+            throw new Exception("Not yet implemented!");
+        }
+    }
+
+    enum NailShapes
+    {
+        Almond,
+        MountainPeak,
+        Lipstick,
+    }
+
+    enum NailPatterns
+    {
+        Fire,
+        Leaf,
+        Stars,
+    }
+}
