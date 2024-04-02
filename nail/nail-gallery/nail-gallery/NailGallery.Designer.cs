d8476db367fcc865231b45172c7decfbc4f79ed7
p4lm4d3v <matejadjokic23@gmail.com>
2024-04-03 01:15:01 +0200

    v0.2.1
    the gallery is almost done
    has pagination control and display
    has correct nails display with correct data
    just needs to turn the data into an image and display it

---
+++ nail/nail-gallery/nail-gallery/NailGallery.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace nail_gallery
+{
+    partial class NailGallery
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grid = new TableLayoutPanel();
+            fullRight = new Button();
+            fullLeft = new Button();
+            left = new Button();
+            right = new Button();
+            pagination = new TableLayoutPanel();
+            indexInput = new TextBox();
+            pagination.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grid
+            // 
+            grid.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            grid.ColumnCount = 2;
+            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            grid.Location = new Point(0, 0);
+            grid.Name = "grid";
+            grid.RowCount = 2;
+            grid.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            grid.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            grid.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            grid.Size = new Size(800, 410);
+            grid.TabIndex = 6;
+            // 
+            // fullRight
+            // 
+            fullRight.Dock = DockStyle.Fill;
+            fullRight.Font = new Font("Microsoft Sans Serif", 15F);
+            fullRight.Location = new Point(323, 3);
+            fullRight.Name = "fullRight";
+            fullRight.Size = new Size(74, 31);
+            fullRight.TabIndex = 1;
+            fullRight.Text = ">>";
+            fullRight.UseVisualStyleBackColor = true;
+            fullRight.Click += FullRightClick;
+            // 
+            // fullLeft
+            // 
+            fullLeft.Dock = DockStyle.Fill;
+            fullLeft.Font = new Font("Microsoft Sans Serif", 15F);
+            fullLeft.Location = new Point(3, 3);
+            fullLeft.Name = "fullLeft";
+            fullLeft.Size = new Size(74, 31);
+            fullLeft.TabIndex = 0;
+            fullLeft.Text = "<<";
+            fullLeft.UseVisualStyleBackColor = true;
+            fullLeft.Click += FullLeftClick;
+            // 
+            // left
+            // 
+            left.Dock = DockStyle.Fill;
+            left.Font = new Font("Microsoft Sans Serif", 15F);
+            left.Location = new Point(83, 3);
+            left.Name = "left";
+            left.Size = new Size(74, 31);
+            left.TabIndex = 1;
+            left.Text = "<";
+            left.UseVisualStyleBackColor = true;
+            left.Click += LeftClick;
+            // 
+            // right
+            // 
+            right.Dock = DockStyle.Fill;
+            right.Font = new Font("Microsoft Sans Serif", 15F);
+            right.Location = new Point(243, 3);
+            right.Name = "right";
+            right.Size = new Size(74, 31);
+            right.TabIndex = 0;
+            right.Text = ">";
+            right.UseVisualStyleBackColor = true;
+            right.Click += RightClick;
+            // 
+            // pagination
+            // 
+            pagination.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pagination.ColumnCount = 5;
+            pagination.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            pagination.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            pagination.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            pagination.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            pagination.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            pagination.Controls.Add(fullLeft, 0, 0);
+            pagination.Controls.Add(left, 1, 0);
+            pagination.Controls.Add(right, 3, 0);
+            pagination.Controls.Add(fullRight, 4, 0);
+            pagination.Controls.Add(indexInput, 2, 0);
+            pagination.Location = new Point(200, 413);
+            pagination.Name = "pagination";
+            pagination.RowCount = 1;
+            pagination.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            pagination.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            pagination.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            pagination.Size = new Size(400, 37);
+            pagination.TabIndex = 7;
+            // 
+            // indexInput
+            // 
+            indexInput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            indexInput.Location = new Point(163, 3);
+            indexInput.MaxLength = 3;
+            indexInput.Multiline = true;
+            indexInput.Name = "indexInput";
+            indexInput.Size = new Size(74, 31);
+            indexInput.TabIndex = 2;
+            indexInput.TextAlign = HorizontalAlignment.Center;
+            indexInput.TextChanged += IndexInputChanged;
+            // 
+            // NailGallery
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pagination);
+            Controls.Add(grid);
+            Name = "NailGallery";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Nail Gallery";
+            Load += NailGalleryLoad;
+            pagination.ResumeLayout(false);
+            pagination.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button right;
+        private Button fullRight;
+        private Button left;
+        private Button fullLeft;
+        private TableLayoutPanel grid;
+        private TableLayoutPanel pagination;
+        private TextBox indexInput;
+    }
+}
---
+++ nail/nail-gallery/nail-gallery/NailGallery.cs
@@ -0,0 +1,218 @@
+using System.Drawing.Design;
+using System.Reflection.Metadata;
+
+namespace nail_gallery
+{
+    public partial class NailGallery : Form
+    {
+        private List<Nail> items;
+        private int currentIndex = 0;
+        private int totalItemCount = 1000;
+        private int rows = 3;
+        private int cols = 4;
+        private int rc;
+        private int modifier;
+
+        public NailGallery()
+        {
+            InitializeComponent();
+
+            this.items = new List<Nail>();
+            this.rc = this.rows * this.cols;
+            this.modifier = items.Count % this.rc == 0 ? 0 : -1;
+
+            // Register keydown event
+            this.KeyDown += MyKeyDown!;
+            this.KeyPreview = true;
+        }
+
+        private void MyKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.I)
+                ShowNailsDialog();
+            else if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+        }
+        private void NailGalleryLoad(object sender, EventArgs e)
+        {
+            InitializeNails();
+            DisplayItems();
+        }
+        private void InitializeNails()
+        {
+            for (int i = 0; i < totalItemCount; i++)
+                items.Add(Nail.Random());
+        }
+        private void DisplayItems()
+        {
+            this.indexInput.Text = $"{currentIndex + 1}";
+
+
+            this.grid.Controls.Clear();
+
+            int startIndex = currentIndex * this.rc;
+            int endIndex = Math.Min(startIndex + this.rc, items.Count);
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                Nail nail = this.items[i];
+                Button button = new Button();
+                button.Text = nail.Info(i);
+                button.TextAlign = ContentAlignment.MiddleLeft;
+                button.Dock = DockStyle.Fill;
+                grid.Controls.Add(button);
+            }
+
+            grid.RowStyles.Clear();
+            grid.RowCount = this.rows;
+            grid.ColumnCount = this.cols;
+            for (int i = 0; i < this.rows; i++)
+                grid.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / this.rows));
+            for (int i = 0; i < this.cols; i++)
+                grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / this.cols));
+        }
+        private void ShowNailsDialog()
+        {
+            string txt = string.Empty;
+            for (int i = 0; i < items.Count; i++)
+            {
+                txt += items[i].Info(i);
+                txt += "\n";
+            }
+            MessageBox.Show(txt, "Nails");
+        }
+        private void IndexInputChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                currentIndex = int.Parse(indexInput.Text) - 1;
+                if (currentIndex < 0 || currentIndex > totalItemCount / this.rc + this.modifier)
+                    throw new Exception();
+                indexInput.ForeColor = Color.Black;
+                DisplayItems();
+            }
+            catch
+            {
+                indexInput.ForeColor = Color.Red;
+            }
+        }
+        private void FullLeftClick(object sender, EventArgs e)
+        {
+
+            if (currentIndex != 0)
+            {
+                currentIndex = 0;
+                DisplayItems();
+            }
+        }
+        private void LeftClick(object sender, EventArgs e)
+        {
+
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                DisplayItems();
+            }
+        }
+        private void RightClick(object sender, EventArgs e)
+        {
+
+            if (currentIndex < (items.Count / this.rc) + modifier)
+            {
+                currentIndex++;
+                DisplayItems();
+            }
+        }
+        private void FullRightClick(object sender, EventArgs e)
+        {
+
+            if (currentIndex != items.Count / this.rc + modifier)
+            {
+                currentIndex = items.Count / this.rc + modifier;
+                DisplayItems();
+            }
+        }
+    }
+
+    public class Nail
+    {
+        public NailShape shape;
+        public NailPattern pattern;
+        public Color shapeColor;
+        public Color patternColor;
+        public Nail(
+            NailShape shape = NailShape.Almond,
+            NailPattern pattern = NailPattern.FireStyle,
+            Color nailColor = default,
+            Color patternColor = default)
+        {
+            this.shape = shape;
+            this.pattern = pattern;
+            this.shapeColor = nailColor;
+            this.patternColor = patternColor;
+        }
+        public String Info(int index)
+        {
+            Color sc = this.shapeColor;
+            Color pc = this.patternColor;
+            string nailColorTxt = $"rgb({sc.R},{sc.G},{sc.B})";
+            string patternColorTxt = $"rgb({pc.R},{pc.G},{pc.B})";
+            return $"Nail {index + 1}:\n    Shape - {this.shape}\n    Pattern - {this.pattern}\n    NailColor - {nailColorTxt}\n    PatternColor - {patternColorTxt}";
+        }
+        public static Nail Random()
+        {
+            return new Nail(
+                shape: Nail.RandomShape(),
+                pattern: Nail.RandomPattern(),
+                nailColor: Nail.RandomColor(),
+                patternColor: Nail.RandomColor()
+            );
+        }
+        private static NailShape RandomShape()
+        {
+            return (NailShape)Nail.RandomIndexForEnum<NailShape>();
+        }
+        private static NailPattern RandomPattern()
+        {
+            return (NailPattern)Nail.RandomIndexForEnum<NailPattern>();
+        }
+        private static int RandomIndexForEnum<T>()
+        {
+            Random rnd = new Random();
+            int enumLength = Enum.GetValues(typeof(T)).Length;
+            return rnd.Next(enumLength);
+        }
+        private static Color RandomColor()
+        {
+            Random rnd = new Random();
+            return Color.FromArgb(
+                red: rnd.Next(256),
+                green: rnd.Next(256),
+                blue: rnd.Next(256)
+            );
+        }
+    }
+
+    public enum NailShape
+    {
+        Almond,
+        Ballerina,
+        Lipstick,
+        MountainPeak,
+        Oval,
+        Rounded,
+        Short,
+        Squoval,
+        Stiletto,
+        Wide
+    }
+
+    public enum NailPattern
+    {
+        FireStyle,
+        LeafStyle,
+        StarsStyle,
+        HeartsStyle,
+        LeavesStyle
+    }
+}
